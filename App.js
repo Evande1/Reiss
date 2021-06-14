96c4540b7d6bcc2be6902c8c4c13633eb6a59a0c
TJun-Jie <tjunjie2000@gmail.com>
2021-06-14 12:34:32 +0800

    added some test code

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app test! test</Text>
+      <Text> some bad codet</Text>
       <StatusBar style='auto' />
     </View>
   );
