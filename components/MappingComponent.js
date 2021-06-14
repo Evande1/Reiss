3edb3417f2437a3b0cc2b7cc9102da7f68703a89
TJun-Jie <tjunjie2000@gmail.com>
2021-06-15 01:35:40 +0800

    Added distance and used location as starting point of map

--- components/MappingComponent.js
+++ components/MappingComponent.js
@@ -2,34 +2,48 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
 import MapView, { Marker } from 'react-native-maps';
+import { ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
 
-const GOOGLE_MAPS_APIKEY = 'AIzaSyDtFrz2OwplStRTnHSBIsljuL5Oh9XNoTA';
-const origin = { latitude: 1.3584168333017268, longitude: 103.70746290442666 };
-const destination = {
-  latitude: 1.3500577333970956,
-  longitude: 103.72828007393781,
-};
-const secondDestination = {
-  latitude: 1.3316125813561035,
-  longitude: 103.72105779063803,
-};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBc4I3OelmxwirxU-Q_yjhPqmjwPE86JE8';
 
 function MappingComponent() {
+  // loading state
+  const [loading, setLoading] = useState(null);
+  // distance state
+  const [distance, setDistance] = useState(0);
+
+  const [markers, setMarkers] = useState([]);
+  const [directions, setDirections] = useState();
+  const renderedDirections = [];
+  const renderedMarker = [];
+
+  // this whole portion below is to get the current location of the person.  I console.log the data so you can see it in the terminal.
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+  const getLocation = async () => {
+    setLoading(true);
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      setLoading(false);
+      return;
+    }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    setMarkers([
+      {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      },
+    ]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getLocation();
   }, []);
   let text = 'Waiting..';
   if (errorMsg) {
@@ -38,65 +52,68 @@ function MappingComponent() {
     text = JSON.stringify(location);
   }
   // get current location
-  console.log(text);
+  // console.log(text);
 
-  const [markers, setMarkers] = useState([
-    origin,
-    destination,
-    secondDestination,
-  ]);
-  const renderedMarker = [];
-  for (let i = 0; i < markers.length; i++) {
-    renderedMarker.push(<Marker key={i} coordinate={markers[i]}></Marker>);
-  }
-
-  const [directions, setDirections] = useState();
-  const renderedDirections = [];
-  for (let i = 0; i < markers.length; i++) {
-    // only push if there is more than  1 marker
-    if (markers.length > 1 && i < markers.length) {
-      renderedDirections.push(
-        <MapViewDirections
-          key={i}
-          origin={markers[i]}
-          destination={markers[i + 1]}
-          apikey={GOOGLE_MAPS_APIKEY}
-        />
-      );
+  if (markers.length > 0) {
+    for (let i = 0; i < markers.length; i++) {
+      renderedMarker.push(<Marker key={i} coordinate={markers[i]}></Marker>);
     }
+
+    for (let i = 0; i < markers.length; i++) {
+      // only push if there is more than  1 marker and less than the max number of markers.
+      // if got 3 markers, means there will be 2 destination. From A to B and B to C
+      // If only got 1 marker, means there is no destination.
+      if (markers.length > 1 && i < markers.length) {
+        renderedDirections.push(
+          <MapViewDirections
+            key={i}
+            origin={markers[i]}
+            destination={markers[i + 1]}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={4}
+            strokeColor={'#007fff'}
+            onReady={(result) => setDistance(distance + result.distance)}
+          />
+        );
+      }
+    }
+  }
+  if (text === 'Waiting..') {
+    return (
+      <View>
+        <ActivityIndicator />
+      </View>
+    );
   }
 
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 1.3584168333017268,
-        longitude: 103.70746290442666,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      }}
-      onPress={(event) =>
-        setMarkers([...markers, event.nativeEvent.coordinate])
-      }
-    >
-      {/* <MapViewDirections
-        origin={origin}
-        destination={destination}
-        apikey={GOOGLE_MAPS_APIKEY}
-      />
-      <MapViewDirections
-        origin={destination}
-        destination={secondDestination}
-        apikey={GOOGLE_MAPS_APIKEY}
-      /> */}
-      {renderedDirections}
-      {renderedMarker}
-
-      {/* <Marker coordinate={origin} />
-      <Marker coordinate={destination} />
-      <Marker coordinate={secondDestination} /> */}
-      {/* onPress = {(event) => console.log('test')} */}
-    </MapView>
+    <View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        onPress={(event) =>
+          setMarkers([...markers, event.nativeEvent.coordinate])
+        }
+      >
+        {/* example of MapViewDirections */}
+        {/* <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+        /> */}
+        {renderedDirections}
+        {renderedMarker}
+        {/* Example of marker coordinate */}
+        {/* <Marker coordinate={origin} /> */}
+      </MapView>
+      {/* need to add further styling */}
+      <Text>Distance: {distance}</Text>
+    </View>
   );
 }
 
@@ -109,7 +126,8 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    // 80% of the screen takes on the height. so there is 20% space left at the bottom for you to fit things in.
+    height: Dimensions.get('window').height * 0.8,
   },
 });
 
--- screens/MappingScreen.js
+++ screens/MappingScreen.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Modal } from 'react-native';
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
+import MappingComponent from '../components/MappingComponent';
 
 function MappingScreen({ navigation }) {
   return (
     <View stylexe={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <MappingComponent></MappingComponent>
       <Button onPress={() => navigation.goBack()} title='Go back home' />
     </View>
   );
 }
 
 export default MappingScreen;
-
