2a7bfcce0dcead5ea3fe2a843976164ce6ed1271
TJun-Jie <tjunjie2000@gmail.com>
2021-06-15 19:21:44 +0800

    added map

---
+++ components/MapComponent.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCve7vFBOjV4kK_DwhSHnn2JF-hXAPeszw';
+
+const origin = { latitude: 1.3584168333017268, longitude: 103.70746290442666 };
+const destination = {
+  latitude: 1.3500577333970956,
+  longitude: 103.72828007393781,
+};
+const secondDestination = {
+  latitude: 1.3316125813561035,
+  longitude: 103.72105779063803,
+};
+
+function MapComponent() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  // get current location
+  console.log(text);
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 1.3584168333017268,
+        longitude: 103.70746290442666,
+        latitudeDelta: 0.08,
+        longitudeDelta: 0.08,
+      }}
+    >
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={4}
+        strokeColor={'#007fff'}
+      />
+      <MapViewDirections
+        origin={destination}
+        destination={secondDestination}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={4}
+        strokeColor={'#007fff'}
+      />
+      <Marker coordinate={origin} />
+      <Marker coordinate={destination} />
+      <Marker coordinate={secondDestination} />
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height * 0.6,
+  },
+});
+
+export default MapComponent;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,18 +7,22 @@ import {
   Modal,
   TouchableOpacity,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SelectRoute from '../components/SelectRoute';
-import ComponentButton from '../components/ComponentButton';
+import StartButton from '../components/StartButton';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen({ navigation }) {
   const [inModalMode, setInModalMode] = useState(false);
 
   const cancelInModalMode = () => setInModalMode(false);
+
+  const SelectRoutes = () => {
+    cancelInModalMode();
+    navigation.navigate('PastRoutes');
+  };
 
   const username = 'JYP';
   const text1 = `Good Morning ${username}`.toUpperCase();
@@ -26,16 +30,12 @@ function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}> App Name</Text>
-      </View>
-
       <View style={styles.startPopUp}>
         <View>
           <Text style={styles.startPopUpText1}>{text1}</Text>
           <Text style={styles.startPopUpText2}>{text2}</Text>
         </View>
-        <ComponentButton
+        <StartButton
           text='Start'
           color='black'
           onPress={() => setInModalMode(true)}
@@ -46,14 +46,21 @@ function HomeScreen({ navigation }) {
   );
 }
 
+function PastRoutes() {
+  return <Text>Past Routes Screen</Text>;
+}
+
+function MappedRoutes() {
+  return <Text>Mapped Routes Screen</Text>;
+}
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: 15,
-    padding: 20,
+    padding: 5,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
   },
 
   header: {
@@ -72,22 +79,25 @@ const styles = StyleSheet.create({
 
   startPopUp: {
     width: '100%',
-    maxHeight: '30%',
-    backgroundColor: '#f48731',
+    maxHeight: '90%',
+    backgroundColor: 'white',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    justifyContent: 'space-between',
   },
 
   startPopUpText1: {
     color: 'black',
     fontWeight: 'bold',
-    fontSize: 30,
+    textAlign: 'center',
+    fontSize: 25,
   },
 
   startPopUpText2: {
     color: 'black',
-    fontSize: 20,
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight: '300',
   },
 
   startButton: {
@@ -98,6 +108,41 @@ const styles = StyleSheet.create({
     borderRadius: 30,
   },
 });
+
+const Stack = createStackNavigator();
+
+export default function HomeScreenStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          headerLeft: () => (
+            <Icon.Button
+              name='ios-menu'
+              size={25}
+              backgroundColor='#009387'
+              onPress={() => navigation.openDrawer()}
+            ></Icon.Button>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 // function HomeScreen({ navigation }) {
 //   return (
@@ -110,35 +155,36 @@ const styles = StyleSheet.create({
 //     </View>
 //   );
 // }
-const HomeStack = createStackNavigator();
-const HomeStackScreen = ({ navigation }) => (
-  <HomeStack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#009387',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    }}
-  >
-    <HomeStack.Screen
-      name='Home'
-      component={HomeScreen}
-      options={{
-        title: 'Home',
-        headerLeft: () => (
-          <Icon.Button
-            name='ios-menu'
-            size={25}
-            backgroundColor='#009387'
-            onPress={() => navigation.openDrawer()}
-          ></Icon.Button>
-        ),
-      }}
-    />
-  </HomeStack.Navigator>
-);
 
-export default HomeStackScreen;
+// const HomeStack = createStackNavigator();
+// const HomeStackScreen = ({ navigation }) => (
+//   <HomeStack.Navigator
+//     screenOptions={{
+//       headerStyle: {
+//         backgroundColor: '#009387',
+//       },
+//       headerTintColor: '#fff',
+//       headerTitleStyle: {
+//         fontWeight: 'bold',
+//       },
+//     }}
+//   >
+//     <HomeStack.Screen
+//       name='Home'
+//       component={HomeScreen}
+//       options={{
+//         title: 'Home',
+//         headerLeft: () => (
+//           <Icon.Button
+//             name='ios-menu'
+//             size={25}
+//             backgroundColor='#009387'
+//             onPress={() => navigation.openDrawer()}
+//           ></Icon.Button>
+//         ),
+//       }}
+//     />
+//   </HomeStack.Navigator>
+// );
+
+// export default HomeStackScreen;
