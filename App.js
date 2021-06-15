5eb5c21f0fec8e967a960e4d225ad84ba758ed03
jinyangp <jinyangpeh@gmail.com>
2021-06-15 12:12:12 +0800

    Modal and Screen for Route options at Start Button

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
-<<<<<<< Updated upstream
-import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-=======
 import * as React from "react";
 import { Button, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
->>>>>>> Stashed changes
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Icon from "react-native-vector-icons/Ionicons";
 
-<<<<<<< Updated upstream
-import HomeScreen from './screens/HomeScreen';
-import FeedScreen from './screens/FeedScreen';
-import HistoryScreen from './screens/HistoryScreen';
-import ResultScreen from './screens/ResultScreen';
-import MappingScreen from './screens/MappingScreen';
-import TestScreen from './screens/TestScreen';
-=======
 import HomeScreen from "./screens/HomeScreen";
 import FeedScreen from "./screens/FeedScreen";
 import HistoryScreen from "./screens/HistoryScreen";
 import ResultScreen from "./screens/ResultScreen";
 import MappingScreen from "./screens/MappingScreen";
->>>>>>> Stashed changes
 
 const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
@@ -35,105 +19,6 @@ const HistoryStack = createStackNavigator();
 const MappingStack = createStackNavigator();
 const ResultStack = createStackNavigator();
 
-<<<<<<< Updated upstream
-const HomeStackScreen = ({navigation}) => (
-  <HomeStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: "#009387",
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: 'bold'
-    }
-  }}>
-    <HomeStack.Screen name ='Home' component={HomeScreen} options={{
-      title: 'Home',
-      headerLeft: () => (
-        <Icon.Button name ='ios-menu' size ={25} backgroundColor="#009387"
-        onPress={() => navigation.openDrawer()}></Icon.Button>
-      )
-    }} />
-  </HomeStack.Navigator>
-);
-
-const FeedStackScreen = ({navigation}) => (
-  <FeedStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: "#009387",
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: 'bold'
-    }
-  }}>
-    <FeedStack.Screen name ='Feed' component={FeedScreen} options={{
-      title: 'Feed',
-      headerLeft: () => (
-        <Icon.Button name ='ios-menu' size ={25} backgroundColor="#009387"
-        onPress={() => navigation.openDrawer()}></Icon.Button>
-      )
-    }} />
-  </FeedStack.Navigator>
-);
-
-const HistoryStackScreen = ({navigation}) => (
-  <HistoryStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: "#009387",
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: 'bold'
-    }
-  }}>
-    <HistoryStack.Screen name ='History' component={HistoryScreen} options={{
-      title: 'History',
-      headerLeft: () => (
-        <Icon.Button name ='ios-menu' size ={25} backgroundColor="#009387"
-        onPress={() => navigation.openDrawer()}></Icon.Button>
-      )
-    }} />
-  </HistoryStack.Navigator>
-);
-
-const MappingStackScreen = ({navigation}) => (
-  <MappingStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: "#009387",
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: 'bold'
-    }
-  }}>
-    <MappingStack.Screen name ='?' component={MappingScreen} options={{
-      title: 'Mapping',
-      headerLeft: () => (
-        <Icon.Button name ='ios-menu' size ={25} backgroundColor="#009387"
-        onPress={() => navigation.openDrawer()}></Icon.Button>
-      )
-    }} />
-  </MappingStack.Navigator>
-);
-
-const ResultStackScreen = ({navigation}) => (
-  <ResultStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: "#009387",
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: 'bold'
-    }
-  }}>
-    <ResultStack.Screen name ='Result' component={ResultScreen} options={{
-      title: 'Result',
-      headerLeft: () => (
-        <Icon.Button name ='ios-menu' size ={25} backgroundColor="#009387"
-        onPress={() => navigation.openDrawer()}></Icon.Button>
-      )
-    }} />
-=======
 const HomeStackScreen = ({ navigation }) => (
   <HomeStack.Navigator
     screenOptions={{
@@ -281,7 +166,6 @@ const ResultStackScreen = ({ navigation }) => (
         ),
       }}
     />
->>>>>>> Stashed changes
   </ResultStack.Navigator>
 );
 
